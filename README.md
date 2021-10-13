5b9fe24164592ca69ddaba91bb12d3ba0665b568
RODRIGO ROTH VASCONCELLOS <67717000+rodrigoroth@users.noreply.github.com>
2021-10-12 21:31:50 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# solvimm-aws-serverless
+# Solvimm Serverless Challenge
+
+This project consists on a serverless operation, that everytime an image is uploaded on s3 bucket
+it has its metadata extracted and stored on dynamodb (so it can be used later), and generates a
+endpoint via aws api gateway to check data from the object stored.
+
+## Installation
+
+The serverless.yml plugin sometimes doesn't work, so you have to install it manually:
+
+```bash
+serverless plugin install -n serverless-python-requirements
+```
+
+Since it uses Pillow to read the image extracted, the only way that I finded to deploy it as a layer
+was following this [tutorial](https://forums.aws.amazon.com/thread.jspa?threadID=309588).
+
+
+
+## Usage
+
+For deploy and lambda function usage, run the following command:
+
+```bash
+serverless deploy
+```
+
+To use the info_images function to get data from the database, run:
+
+```bash
+python handler.py 
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Author
+
+[Rodrigo Roth Vasconcellos](https://www.linkedin.com/in/rodrigo-roth-vasconcellos-815377106/)
+
--- handler.py
+++ handler.py
@@ -1,23 +1,33 @@
-import boto3
 import os
-from PIL import Image
 import json
-from boto3.dynamodb.conditions import Key
+import boto3
+from PIL import Image
+from collections import Counter
 
-s3 = boto3.client('s3')
+s3 = boto3.client('s3', region_name="us-east-1")
+dynamodb = boto3.client('dynamodb', region_name="us-east-1")
+resource = boto3.resource('dynamodb', region_name="us-east-1")
+table = resource.Table('serverless-challenge-dev')
 
 
 def extract_metadata(event, context):
-    print(event)
+    """
+    This function invokes a lambda function that access s3 everytime an image is uploaded, then return its metadata
+    (key, size, height and width) and stores them on dynamodb serverless-challenge-dev table
 
-    bucket = event['Records'][0]['s3']['bucket']['name']
+    :param event: event source invoked from lambda function that access s3 and return the object metadata
+    :type event: object creation
+    :param context: lambda function location
+    :type context: lambda function
+    :return: returns size, height and width from the image uploaded on the database
+    :rtype: dict
+
+    """
     key = event['Records'][0]['s3']['object']['key']
     size = event['Records'][0]['s3']['object']['size']
-    dynamodb = boto3.client('dynamodb')
 
-    local = '/tmp/' + os.path.basename(key)
-    s3.download_file(bucket, key, local)
-    im = Image.read(local)
+    local = get_image(event)
+    im = Image.open(local)
     width, height = im.size
 
     dynamodb.put_item(
@@ -28,35 +38,94 @@ def extract_metadata(event, context):
             'height': {'N': str(height)},
             'width': {'N': str(width)}
         })
+    return {
+        "statusCode": 200,
+        "body": {
+            "size": str(size),
+            "height": str(height),
+            "width": str(width)
+        }
+    }
 
 
 def get_metadata(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    # print(event)
+    """
+    This function invokes a lambda function that generates an endpoint for AWS API Gateway created from the s3objectkey
+    image uploaded on the database
+    
+    :param event: event source invoked from lambda function that access dynamodb
+    :type event: object response
+    :param context: lambda function location
+    :type context: lambda function
+    :return: returns the metadata from the item stored on the database
+    :rtype: dict
+    """
     key = event['pathParameters']['s3objectkey']
     data = 'uploads/' + key
-    table = dynamodb.Table('serverless-challenge-dev')
     response = table.get_item(
         Key={
             's3objectkey': data
         }
     )
-    # print(response)
+    print(response['Item'])
     return {
         'statusCode': 200,
-        'body': response['Item']
+        'headers': {'Content-Type': 'application/json'},
+        's3objectkey': response['Item']
     }
 
 
 def get_image(s3objectkey):
-    print("Hello World")
+    """
+    This function receives the s3objectkey from the lambda function extract_metadata event and downloads it so it can
+    have its metadata extracted
+    
+    :param s3objectkey: key used to identify the image uploaded on s3 so it can be downloaded temporarily
+    :type s3objectkey: str
+    :return: returns the local where the image is temporarily downloaded
+    :rtype: str
+    """
+    bucket = s3objectkey['Records'][0]['s3']['bucket']['name']
+    key = s3objectkey['Records'][0]['s3']['object']['key']
+
+    local = os.path.join('/tmp/', os.path.basename(key))
+    s3.download_file(bucket, key, local)
+    return local
 
 
 def info_images():
-    print("Hello World")
+    """
+    This function scans the table from dynamodb
+    
+    :return: returns the biggest and smallest size from all items, the total amount of items and all the items different
+    types
+    :rtype: dict
+    """
+    all_items = []
+    result_data = table.scan()
+    all_items.extend(result_data['Items'])
+    while 'LastEvaluatedKey' in result_data:
+        result_data = table.scan(ExclusiveStartKey=result_data['LastEvaluatedKey'])
+        all_items.extend(result_data['Items'])
+
+    size_list = [int(obj["size"]) for obj in all_items]
+    type_list = [obj["s3objectkey"].split(".")[-1] for obj in all_items]
+    max_size = max(size_list)
+    min_size = min(size_list)
+    item_count = len(size_list)
+
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'result': {
+            "max_size": max_size,
+            "min_size": min_size,
+            "item_count": item_count,
+            "type_count": dict(Counter(type_list))
+        }
+    }
 
 
 if __name__ == '__main__':
-    with open('/home/rodrigo/PycharmProjects/pythonProject/solvimmproject/getdatatempleta.json', 'r') as f:
-        template = json.load(f)
-    get_metadata(template, "")
+    result = info_images()
+    print(result)
